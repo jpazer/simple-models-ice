9e339fb510cc5b46a3536831537ad0714b7dd279
jpazer <jgp5980@rit.edu>
2016-03-15 14:41:37 -0400

    adding files

---
+++ src/models/Dog.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+var DogModel;
+
+var DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    breed: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    age: {
+        type: Number,
+        min: 1,
+        required: true
+    },
+
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+DogSchema.methods.sayName = function() {
+    console.log(this.name);
+};
+
+DogSchema.statics.findByName = function(name, callback) {
+
+    var search = {
+        name: name
+    };
+
+    return DogModel.findOne(search, callback);
+};
+
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+//export our public properties
+module.exports.CatModel = DogModel;
+module.exports.CatSchema = DogSchema;
